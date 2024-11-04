fe32a1885779372f69ffbefd5567650dc9fea29f
rishab <rishabstudent2000@gmail.com>
2024-11-04 23:18:31 +0530

    Deploy

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(7070, () => console.log('Server started on port 7070'));
+    app.listen(8080, () => console.log('Server started on port 8080'));
   } catch (error) {
     console.log(error);
   }
